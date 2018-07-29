ad9ca7afb14c4f5f1696664f9a7310fe17df8ae1
sue young <sueyoung@yahoo.com>
2018-07-28 20:19:16 -0700

    Task 2.3 – JavaScript implementations

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log("Hello! Welcome to my portfolio site.")
+
+$(document).ready(function() {
+  $(".project").on("mouseenter", function() {
+    $(this).find(".project-details").fadeIn();
+  });
+  $(".project").on("mouseleave", function() {
+    $(this).find(".project-details").fadeOut();
+  });
+
+  //  $(".project-preview").on("click", function() {
+  //    $(this).siblings(".project-details").slideToggle();
+});
